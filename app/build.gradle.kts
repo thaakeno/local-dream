03e62ac28eaf6705b7722a114bde998a35dceb33
thaakeno <thaakeno46@gmail.com>
2026-09-26 23:16:06 +0200

    chore: bump redesigned model cards to alpha.37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         minSdk = 28
 //        minSdk = 31
         targetSdk = 36
-        versionCode = 107
-        versionName = "3.0.0-alpha.36"
+        versionCode = 108
+        versionName = "3.0.0-alpha.37"
 
         buildConfigField("String", "GIT_SHA", buildConfigString(buildGitSha))
         buildConfigField("String", "GIT_BRANCH", buildConfigString(buildGitBranch))
