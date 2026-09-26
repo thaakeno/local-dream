af2b1e1cc1f30291c2e2057f3683e83d1f493bef
thaakeno <thaakeno46@gmail.com>
2026-09-26 22:42:23 +0200

    chore: bump YuE2 fixes to alpha.36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         minSdk = 28
 //        minSdk = 31
         targetSdk = 36
-        versionCode = 106
-        versionName = "3.0.0-alpha.35"
+        versionCode = 107
+        versionName = "3.0.0-alpha.36"
 
         buildConfigField("String", "GIT_SHA", buildConfigString(buildGitSha))
         buildConfigField("String", "GIT_BRANCH", buildConfigString(buildGitBranch))
