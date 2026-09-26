dd7f4fd5aac9de5e35eb4559f7c9f66e3bcd0b86
thaakeno <thaakeno46@gmail.com>
2026-09-26 16:37:38 +0200

    build: bump alpha.30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         minSdk = 28
 //        minSdk = 31
         targetSdk = 36
-        versionCode = 100
-        versionName = "3.0.0-alpha.29"
+        versionCode = 101
+        versionName = "3.0.0-alpha.30"
 
         buildConfigField("String", "GIT_SHA", buildConfigString(buildGitSha))
         buildConfigField("String", "GIT_BRANCH", buildConfigString(buildGitBranch))
